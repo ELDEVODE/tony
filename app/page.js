fff1b648f222761e6e52e30d897ba34b7cb2f56a
El-Praise Ayo <elpraise20@gmail.com>
2024-09-14 15:39:20 +0100

    refactor: Optimize background image rendering in Home component

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./Hero";
@@ -6,60 +7,96 @@ import EggEffect from "./components/EggEffect";
 import Pokers from "./Pokers";
 import Section1 from "./Section1";
 import Tokenomics from "./Tokenomics";
-import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+
+const backgrounds = [
+  "/images/hall.jpg",
+  "/images/t2.jpg",
+  "/images/two.jpg", // Add more background images as needed
+  // "/images/background4.jpg",
+];
 
 export default function Home() {
+  const [activeBackground, setActiveBackground] = useState(0);
+  const sectionRefs = useRef([]);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observerCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = sectionRefs.current.indexOf(entry.target);
+          setActiveBackground(index);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+
+    sectionRefs.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      sectionRefs.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
+
   return (
-    <ParallaxProvider>
-      <main className="relative">
-        <link
-          href="https://fonts.cdnfonts.com/css/la-vie-boheme"
-          rel="stylesheet"
-        />
+    <main className="relative">
+      <link
+        href="https://fonts.cdnfonts.com/css/la-vie-boheme"
+        rel="stylesheet"
+      />
 
-        {/* Hero section with parallax background */}
-        <div className="relative">
-          {/* Background Parallax Image for Hero */}
-          <Parallax className="absolute inset-0 z-0" speed={10}>
-            <Image
-              src="/images/hall.jpg"
-              alt="Hero Background"
-              layout="fill"
-              objectFit="cover"
-              className="blur-[3px]"
-            />
-          </Parallax>
-
-          {/* Hero content */}
-          <div className="relative z-10">
-            <Navbar />
-            <Hero />
-          </div>
+      {/* Background images */}
+      {backgrounds.map((bg, index) => (
+        <div
+          key={bg}
+          className={`fixed inset-0 transition-opacity duration-1000 ${
+            index === activeBackground ? "opacity-100" : "opacity-0"
+          }`}
+          style={{
+            zIndex: -1,
+          }}
+        >
+          <Image
+            src={bg}
+            alt={`Background ${index + 1}`}
+            layout="fill"
+            objectFit="cover"
+            className="blur-[2px]"
+          />
         </div>
+      ))}
 
+      {/* Content sections */}
+      <section ref={(el) => (sectionRefs.current[0] = el)}>
+        <Navbar />
+        <Hero />
         <Pokers />
+      </section>
 
-        {/* Section1 with parallax background */}
-        <div className="relative">
-          {/* Background Parallax Image for Section1 */}
-          <Parallax className="absolute inset-0 z-0" speed={5}>
-            <Image
-              src="/images/two.jpg"
-              alt="Section1 Background"
-              layout="fill"
-              objectFit="cover"
-              className="blur-[3px]"
-            />
-          </Parallax>
+      <section ref={(el) => (sectionRefs.current[1] = el)}>
+        <Section1 />
+      </section>
 
-          {/* Section1 content */}
-          <div className="relative z-10">
-            <Section1 />
-          </div>
-        </div>
-
+      <section ref={(el) => (sectionRefs.current[2] = el)}>
         <Tokenomics />
-      </main>
-    </ParallaxProvider>
+      </section>
+
+      {/* <section ref={(el) => (sectionRefs.current[3] = el)}>
+        
+      </section> */}
+    </main>
   );
 }
